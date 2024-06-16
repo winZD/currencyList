26fbd9ce828a6bde6b7ab193d5039b5ccef75930
winZD <puntica007@gmail.com>
2024-06-16 14:27:45 +0200

    added initial sort by country

--- src/currencyList/CurrencyList.tsx
+++ src/currencyList/CurrencyList.tsx
@@ -1,13 +1,66 @@
 import { useEffect, useState } from "react";
 import { Currency } from "../api/models/currency";
 import { getCurrency } from "../api/currencyList";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./currencyList.css";
 
 function CurrencyList() {
   const [currencies, setCurrencies] = useState<Currency[]>([]);
   const [date, setDate] = useState(new Date());
+  const [searchValue, setSearchValue] = useState("");
+  const [sortField, setSortField] = useState("");
+  const [order, setOrder] = useState("none");
+  const navigate = useNavigate();
 
+  const toggleSort = (field: string) => {
+    if (sortField === field) {
+      setOrder(order === "asc" ? "desc" : "asc");
+    } else {
+      setSortField(field);
+      setOrder("asc");
+    }
+  };
+  console.log(order);
+  const handleSearch = (data: Currency[]): Currency[] => {
+    if (!searchValue) return data;
+
+    const filteredData = data.filter(
+      (item) =>
+        item.drzava.toLowerCase().includes(searchValue.toLowerCase()) ||
+        item.drzava_iso.toLowerCase().includes(searchValue.toLowerCase()) ||
+        item.sifra_valute.toLowerCase().includes(searchValue.toLowerCase()) ||
+        item.valuta.toLowerCase().includes(searchValue.toLowerCase())
+    );
+
+    return filteredData;
+  };
+  const sortData = (data: Currency[]): Currency[] => {
+    const d = data;
+    if (sortField === "drzava") {
+      return order === "asc"
+        ? d.sort((a, b) => {
+            return a.drzava.localeCompare(b.drzava);
+          })
+        : d.sort((a, b) => {
+            return b.drzava.localeCompare(a.drzava);
+          });
+    }
+    return d;
+  };
+
+  /*  const handleSorting = (sortField: string, sortOrder) => {
+    if (sortField) {
+      const sorted = [...tableData].sort((a, b) => {
+        return (
+          a[sortField].toString().localeCompare(b[sortField].toString(), "en", {
+            numeric: true,
+          }) * (sortOrder === "asc" ? 1 : -1)
+        );
+      });
+      setTableData(sorted);
+    }
+  };
+ */
   useEffect(() => {
     getCurrency().then((data) => {
       setCurrencies(data);
@@ -15,11 +68,21 @@ function CurrencyList() {
   }, []);
   return (
     <>
-      <input
-        value={date.toISOString().substring(0, 10)}
-        type="date"
-        onChange={(e) => setDate(new Date(e.target.value))}
-      ></input>{" "}
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Pretraga"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+      </div>
+      <div className="input-container">
+        <input
+          value={date.toISOString().substring(0, 10)}
+          type="date"
+          onChange={(e) => setDate(new Date(e.target.value))}
+        ></input>
+      </div>{" "}
       <button
         onClick={() => setDate(new Date(date.setDate(date.getDate() - 1)))}
       >
@@ -38,8 +101,12 @@ function CurrencyList() {
         <thead>
           <tr>
             <th>Broj tečajnice</th>
-            <th>Datum primjene</th>
+            <th>Datum primjene </th>
+
             <th></th>
+            <th onClick={() => toggleSort("drzava")}>
+              {sortField === "drzava" && (order === "asc" ? "▲" : "▼")}
+            </th>
             <th></th>
             <th></th>
             <th></th>
@@ -52,12 +119,24 @@ function CurrencyList() {
           </tr>
         </thead>
         <tbody>
-          {currencies.map((value, key) => {
+          {handleSearch(sortData(currencies)).map((value, key) => {
             return (
               <tr key={key}>
                 <td>{value.broj_tecajnice}</td>
-                <td>{value.valuta}</td>
                 <td>{value.datum_primjene}</td>
+                <td
+                  onClick={() =>
+                    navigate(
+                      `/povijest/${value.valuta}/${date
+                        .toISOString()
+                        .substring(0, 10)}`
+                    )
+                  }
+                  style={{ cursor: "pointer" }}
+                >
+                  {value.valuta}
+                </td>
+                <td>{value.drzava}</td>
                 <td>{value.broj_tecajnice}</td>
                 <td>{value.kupovni_tecaj}</td>
                 <td>{value.srednji_tecaj}</td>
