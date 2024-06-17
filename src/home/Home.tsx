d862773cc26f6c12a03f5134a7d1d53f8e4effae
winZD <puntica007@gmail.com>
2024-06-17 14:32:00 +0200

    Added css to home screen

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom";
+import "./home.css";
 
 const CURRENCY = "USD";
 
 function Home() {
   return (
-    <div>
-      <Link to="/tecaj">
-        <button>Tečaj</button>
-      </Link>
-      <Link to={`/povijest/${CURRENCY}`}>
-        <button>Povijest</button>
-      </Link>
+    <div className="container">
+      <div className="card">
+        <Link to="/tecaj">
+          <button>Tečaj</button>
+        </Link>
+        <Link to={`/povijest/${CURRENCY}`}>
+          <button>Povijest</button>
+        </Link>
+      </div>
     </div>
   );
 }
